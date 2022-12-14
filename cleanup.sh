7d2ac764ac56a97d0544fe25ce10cebaf6d46adb
Kevin Aird <kevinaird@gmail.com>
2022-12-14 10:55:13 -0500

    fix to cleanup script

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,4 @@
-docker logs --tail 100 simple-stats > ${{ github.run_id }}/simple-stats.log 2>&1
-docker logs --tail 100 jmeter > ${{ github.run_id }}/jmeter.log 2>&1
-docker logs --tail 100 influx > ${{ github.run_id }}/influx.log 2>&1
+echo "runId=$1"
+docker logs --tail 100 simple-stats > $1/simple-stats.log 2>&1
+docker logs --tail 100 jmeter > $1/jmeter.log 2>&1
+docker logs --tail 100 influx > $1/influx.log 2>&1
