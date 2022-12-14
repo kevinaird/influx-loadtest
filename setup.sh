46552cc7dfca475883c44e4b751dbfb03b7d31bd
Kevin Aird <kevinaird@gmail.com>
2022-12-14 09:22:57 -0500

    Added influx only test

--- setup.sh
+++ setup.sh
@@ -2,11 +2,3 @@ echo "Create database in Influx..."
 
 docker exec influx influx -execute "CREATE DATABASE loadrunner"
 
-echo "Create topic in Kafka..."
-
-docker exec kafka kafka-topics.sh \
- --bootstrap-server localhost:9092 \
- --topic loadrunner \
- --create \
- --partitions 3 \
- --replication-factor 1
