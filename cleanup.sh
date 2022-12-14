e96a9f2f2c1f526256e8f95e544444d3feed5879
Kevin Aird <kevinaird@gmail.com>
2022-12-14 11:34:19 -0500

    setup for influx-proxy

--- cleanup.sh
+++ cleanup.sh
@@ -2,6 +2,4 @@ echo "runId=$1"
 docker logs --tail 100 simple-stats > $1/simple-stats.log 2>&1
 docker logs --tail 100 jmeter > $1/jmeter.log 2>&1
 docker logs --tail 100 influx > $1/influx.log 2>&1
-docker logs --tail 100 kafka > $1/kafka.log 2>&1
-docker logs --tail 100 vector > $1/vector.log 2>&1
-docker logs --tail 100 kafka-rest > $1/kafka-rest.log 2>&1
+docker logs --tail 100 influx-proxy > $1/influx-proxy.log 2>&1
